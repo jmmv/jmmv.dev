62eb84975ecfc246c9b96ef27b86a0097b6ce21b
Julio Merino <julio@meroh.net>
2019-11-10 17:25:34 -0500

    Publish rebuilt new site

--- src/wait-for-process-group/wait-all.c
+++ src/wait-for-process-group/wait-all.c
@@ -1,9 +1,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #if defined(WITH_SUBREAPER)
-#include <sys/prctl.h>
+#   include <sys/prctl.h>
 #endif
 
+#include <assert.h>
 #include <err.h>
 #include <errno.h>
 #include <signal.h>
@@ -11,6 +12,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+// Convenience macro to abort quickly if a syscall fails with -1.
+#define CHECK_OK(call) if (call == -1) err(EXIT_FAILURE, #call);
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -24,37 +28,43 @@ int main(int argc, char** argv) {
 #endif
 
     int fds[2];
-    pipe(fds);
+    CHECK_OK(pipe(fds));
+    pid_t pid;
+    CHECK_OK((pid = fork()));
 
-    pid_t pid = fork();
-    if (pid == -1) {
-        err(EXIT_FAILURE, "fork");
-    } else if (pid == 0) {
-        if (setpgid(getpid(), getpid()) == -1) {
-            err(EXIT_FAILURE, "setpgid");
-        }
-        close(fds[0]);
-        write(fds[1], 0, 1);
-        close(fds[1]);
+    if (pid == 0) {
+        // Enter a new process group for all of our descendents.
+        CHECK_OK(setpgid(getpid(), getpid()));
+
+        // Tell the parent that we have successfully created the group.
+        CHECK_OK(close(fds[0]));
+        CHECK_OK(write(fds[1], "\0", sizeof(char)));
+        CHECK_OK(close(fds[1]));
+
+        // Execute the given program now that the environment is ready.
         execv(argv[1], argv + 1);
         err(EXIT_FAILURE, "execv");
     }
 
-    // Arbitrary amount of time to let the child spawn its own subprocess.
-    // This is obviously error-prone but suffices for this example.
-    //sleep(1);
-
-    close(fds[1]);
+    // Wait until the child has created its own process group.
+    //
+    // This is a must to prevent a race between the parent waiting for the
+    // group and the group not existing yet, and is the only safe way to do so.
+    CHECK_OK(close(fds[1]));
     char dummy;
-    read(fds[0], &dummy, 1);
+    CHECK_OK(read(fds[0], &dummy, sizeof(char)));
+    CHECK_OK(close(fds[0]));
 
-    //kill(-pid, SIGTERM);
+    // Wait for the direct child to finish.  We do this separately to collect
+    // and propagate its exit status.
+    int status;
+    CHECK_OK(waitpid(pid, &status, 0));
+
+    // And now wait for any other process in the group to terminate... or not.
     while (waitpid(-pid, NULL, 0) != -1) {
         // Got a child.  Wait for more.
     }
-    if (errno != ECHILD) {
-        err(EXIT_FAILURE, "waitpid");
-    }
-    printf("No children left\n");
-    return EXIT_SUCCESS;
+    assert(errno == ECHILD);
+
+    return WIFEXITED(status) ? WEXITSTATUS(status) : EXIT_FAILURE;
 }
